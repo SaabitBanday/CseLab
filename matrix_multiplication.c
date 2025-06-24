58e03897c55590430ac151052f390fc1d8d42ef2
Saabit Ahmed Banday <saabitisbanday@gmail.com>
2025-06-24 09:39:40 +0530

    Add files via upload

---
+++ matrix_multiplication.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    const char* url = "https://drive.google.com/file/d/1wrZJzMZ2UgGTlc0AkwkJyzf5hBOjjWJG/view?usp=drive_link";
+    printf("Download the Matrix Multiplication file from:\n%s\n", url);
+    return 0;
+}
