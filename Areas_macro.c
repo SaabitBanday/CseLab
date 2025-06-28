5fd3d29db3382b32c7a1e59a1476d569ed3725b1
Saabit Ahmed Banday <saabitisbanday@gmail.com>
2025-06-28 11:19:35 +0530

    Delete Areas_macro.c

--- Areas_macro.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    const char* url = "https://drive.google.com/file/d/1Lmmuzh7rFGAL8eB57To8DXD0nWbySgFv/view?usp=drive_link";
-    printf("Download the Areas Macro file from:\n%s\n", url);
-    return 0;
-}
