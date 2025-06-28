c4ed19a54eda7e23b7dc65726353db64ba0f81d2
Saabit Ahmed Banday <saabitisbanday@gmail.com>
2025-06-28 11:19:52 +0530

    Delete matrix_multiplication.c

--- matrix_multiplication.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    const char* url = "https://drive.google.com/file/d/1wrZJzMZ2UgGTlc0AkwkJyzf5hBOjjWJG/view?usp=drive_link";
-    printf("Download the Matrix Multiplication file from:\n%s\n", url);
-    return 0;
-}
